a28046d7694acd2d469b34ca1ea8572699e28521
harshrajput2804 <161627226+harshrajput2804@users.noreply.github.com>
2025-12-28 22:49:15 +0530

    prompt links add

--- pages/Prompt.py
+++ pages/Prompt.py
@@ -2,4 +2,6 @@ import streamlit as st
 
 st.write("# Get Prompt Websites Links")
 
-
+st.write("Here are some useful websites for finding prompts:")
+st.link_button("Image Generetion Prompts","https://www.bananaprompts.xyz/")
+st.link_button("ChatGPT Prompts templets","https://www.prompthackers.co/")
